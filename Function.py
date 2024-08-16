bcc9172046ba723cf42ec3e7b19d7ac72cbea1f8
pisethsok061021@gmail.com <pisethsok061021@gmail.com>
2024-08-16 23:06:22 +0700

    raw version

--- Function.py
+++ Function.py
@@ -2,23 +2,34 @@
 import time
 import tkinter as tk
 import threading
-import sv_ttk # type: ignore
+import sv_ttk  # type: ignore
 from tkinter import ttk, messagebox
-not_full_screen= False
+import math
+from PIL import Image, ImageTk
+
+not_full_screen=False
+sw_stop_flag=False
+
+def get_img(img, width=20, height=20):
+    start_img = Image.open(img)
+    start_resize = start_img.resize((width, height))
+    final = ImageTk.PhotoImage(start_resize)
+    return final
+
 class Timer:
     def __init__(self, master, root):
-        self.master=master
-        self.root=root
+        self.master = master
+        self.root = root
         self.stop_flag = False
-        
+
     def count_down(self):
         if self.master.minutes.get().isdigit() and self.master.second.get().isdigit():
             self.master.minute_ui.configure(state=tk.DISABLED)
             self.master.second_ui.configure(state=tk.DISABLED)
-            
-            if self.master.minutes.get()=='':
+
+            if self.master.minutes.get() == '':
                 self.master.minutes.set("00")
-            if self.master.second.get()=='':
+            if self.master.second.get() == '':
                 self.master.second.set("00")
 
             for i in range((int(self.master.minutes.get())*60)+int(self.master.second.get())):
@@ -27,11 +38,13 @@ class Timer:
                 if seconds == 0:
                     minutes -= 1
                     self.master.second.set("60")
-                    self.master.minutes.set((f"0{str(minutes)}") if len(str(minutes))==1 else str(minutes))
+                    self.master.minutes.set((f"0{str(minutes)}") if len(
+                        str(minutes)) == 1 else str(minutes))
                     self.root.update_idletasks()
                 else:
                     seconds -= 1
-                    self.master.second.set((f"0{str(seconds)}") if len(str(seconds))==1 else str(seconds))
+                    self.master.second.set((f"0{str(seconds)}") if len(
+                        str(seconds)) == 1 else str(seconds))
                     self.root.update_idletasks()
                 time.sleep(1)
                 if self.stop_flag:
@@ -45,80 +58,95 @@ class Timer:
             self.master.minutes.set("00")
             self.master.second.set("00")
             messagebox.showerror('Error!', 'Please enter only number')
+
     def start(self):
         if self.stop_flag:
-            self.stop_flag=False
+            self.stop_flag = False
         threading.Thread(target=self.count_down).start()
+
     def stop(self):
         if not self.stop_flag:
-            self.stop_flag=True
-    
-    
-
-def stopwatch():
-    pass
-
+            self.stop_flag = True
 
 def change_theme(master, root):
-    
-    
-    global timer_icon, stop_watch, start_icon, stop_icon, timer_icon_white, stop_watch_white, timer_notst, stop_watch_notst
-    
-    #identify tab id
+
+    global timer_icon, stop_watch, start_icon, stop_icon, timer_icon_white, \
+       stop_watch_white, timer_notst, stop_watch_notst, pause_sp, pause_sp_white,reset, reset_white
+
+
+    # identify tab id
     tab_index = int(master.notebook.index(master.notebook.select()))
 
-    #get icon as photoimage
-    timer_icon_white = master.get_img("Assets\\hourglass_white.png", 25, 25)
-    stop_watch_white = master.get_img("Assets\\stopwatch_white.png", 25, 25)
-    timer_icon = master.get_img("Assets\\hourglass.png", 25, 25)
-    stop_watch = master.get_img("Assets\\stopwatch.png", 25, 25)
-    timer_notst=master.get_img("Assets\\hourglass_notst.png", 25, 25)
-    stop_watch_notst=master.get_img("Assets\\stopwatch_notst.png", 25, 25)
-    
-    #apply theme
+    # get icon as photoimage
+    timer_icon_white = get_img("Assets\\hourglass_white.png", )
+    stop_watch_white = get_img("Assets\\stopwatch_white.png", )
+    timer_icon = get_img("Assets\\hourglass.png", )
+    stop_watch = get_img("Assets\\stopwatch.png", )
+    timer_notst = get_img("Assets\\hourglass_notst.png", )
+    stop_watch_notst = get_img("Assets\\stopwatch_notst.png", )
+
+    pause_sp = get_img("Assets\\pause_sp.png",20,20)
+    pause_sp_white = get_img("Assets\\pause_sp_white.png",20,20)
+    reset =get_img("Assets\\reset.png",20,20)
+    reset_white=get_img("Assets\\reset_white.png",20,20)
+
+    # apply theme
     if master.theme_value.get():
-        start_icon = master.get_img(img="Assets\\play_icon_white.png")
-        stop_icon = master.get_img(img="Assets\\x.png")
+        start_icon = get_img(img="Assets\\play_icon_white.png")
+        stop_icon = get_img(img="Assets\\x.png")
         sv_ttk.set_theme("dark")
         color = '#2f2f2f'
-        #config title bar button
+        # config title bar button
         master.close.config(bg=color)
         master.minimize.config(bg=color)
         master.maximize.config(bg=color)
+        customize_style(root, master.theme_value.get())
+        #config stopwatch buttons
+        master.stop_watchBtn.config(image=start_icon)
+        if master.sp_start_flag:
+            master.stop_watchBtn.config(image=pause_sp_white)
+        reset=reset_white
         
-        customize_style(root,master.theme_value.get())
         
-        if tab_index ==0:
-            timer_icon=timer_icon_white
-            stop_watch=stop_watch_notst
-        elif tab_index ==1:
-            stop_watch=stop_watch_white
-            timer_icon=timer_notst
+
+        if tab_index == 0:
+            timer_icon = timer_icon_white
+            stop_watch = stop_watch_notst
+        elif tab_index == 1:
+            stop_watch = stop_watch_white
+            timer_icon = timer_notst
     else:
         sv_ttk.set_theme("light")
         color = '#e7e7e7'
-        #config title bar button
+        # config title bar button
         master.close.config(bg=color)
         master.minimize.config(bg=color)
         master.maximize.config(bg=color)
-        customize_style(root,master.theme_value.get())
-        start_icon = master.get_img(img="Assets\\play-button_724963.png")
-        stop_icon = master.get_img(img="Assets\\pause _icon.png")
-        if tab_index ==0:
-            stop_watch=stop_watch_notst
-        elif tab_index ==1:
-            timer_icon=timer_notst
-    
+        customize_style(root, master.theme_value.get())
+        start_icon = get_img(img="Assets\\play-button_724963.png")
+        stop_icon = get_img(img="Assets\\pause _icon.png")
+        #config stopwatch buttons
+        master.stop_watchBtn.config(image=start_icon)
+        if master.sp_start_flag:
+            master.stop_watchBtn.config(image=pause_sp)
+        
+
+        if tab_index == 0:
+            stop_watch = stop_watch_notst
+        elif tab_index == 1:
+            timer_icon = timer_notst
 
     master.notebook.tab(0, image=timer_icon)
     master.notebook.tab(1, image=stop_watch)
 
     master.start_button.configure(image=start_icon)
     master.stop_button.configure(image=stop_icon)
+    master.reset_button.configure(image=reset)
 
 def customize_style(window, theme):
     style = ttk.Style(window)
-    style.map("TNotebook.Tab",font=[("selected", ("Roboto mono", 15, "bold")), ("!selected", ("Roboto mono", 15))])
+    style.map("TNotebook.Tab", font=[
+              ("selected", ("Roboto mono", 15, "bold")), ("!selected", ("Roboto mono", 15))])
     # remove dotted line around notebook tabs(https://stackoverflow.com/questions/23354303/removing-ttk-notebook-tab-dashed-line?rq=4)
     style.layout("Tab",
                  [('Notebook.tab', {'sticky': 'nswe', 'children':
@@ -134,30 +162,105 @@ def customize_style(window, theme):
         style.map("TNotebook.Tab",  foreground=[("selected", 'white'), ("!selected", '#8b8b8b')],
                   font=[("selected", ("Roboto mono", 15, "bold")), ("!selected", ("Roboto mono", 15))])
     else:
-        style.map("TNotebook.Tab",  foreground=[("selected", 'black'), ("!selected", '#8b8b8b')])
+        style.map("TNotebook.Tab",  foreground=[
+                  ("selected", 'black'), ("!selected", '#8b8b8b')])
     style.configure("TButton", font=("Roboto mono", 12))
     style.configure("Switch.TCheckbutton", font=("Roboto mono", 12))
+
+
 def close_win(root):
     root.after(300, root.destroy())
+
 def maximize_win(root):
     global not_full_screen
     if not_full_screen:
-        root.geometry('900x400')
-        not_full_screen=False
+        root.geometry('900x450')
+        not_full_screen = False
     else:
-        root.geometry(f'{root.winfo_screenwidth()}x{root.winfo_screenheight()}+0+0')
-        not_full_screen=True
+        root.geometry(f'{root.winfo_screenwidth()}x{
+                      root.winfo_screenheight()}+0+0')
+        not_full_screen = True
+
+
 def minimize_window(root):
-   messagebox.showwarning("Sorry!", "You cannot iconify the window")
+    messagebox.showwarning("Sorry!", "You cannot iconify the window")
+
 
 def limit_digit(master, box, second=False):
-    #customize input to only two digit
-    if len(box.get()) ==2:
-        value =box.get()
+    # customize input to only two digit
+    if len(box.get()) == 2:
+        value = box.get()
         out_value = value[0]
         box.set(value[1:len(value)])
         if second:
             master.minute_ui.insert(tk.END, out_value)
-            
-            if len(master.minutes.get())>2:
+
+            if len(master.minutes.get()) > 2:
                 master.minute_ui.delete(0)
+def calculate_path_details(master):
+        # Get the coordinates of the bounding box of the oval path
+        x1, y1, x2, y2 = master.canvas.coords(master.path)
+        # Calculate the center of the circle
+        center_x = (x1 + x2) / 2
+        center_y = (y1 + y2) / 2
+        # Calculate the radius of the circle
+        radius = (x2 - x1) / 2
+        return center_x, center_y, radius
+
+def move_circle(master):
+    if master.is_moving:
+        # Calculate the new position of the circle
+        x = master.center_x + master.radius * math.cos(master.angle)
+        y = master.center_y + master.radius * math.sin(master.angle)
+
+        # Move the circle to the new position
+        circle_radius = 15  
+        master.canvas.coords(master.circle, x - circle_radius, y - circle_radius, x + circle_radius, y + circle_radius)
+        # Increment the angle for the next update
+        master.angle += master.speed
+        # Keep the angle within the range [0, 2*pi]
+        master.angle = master.angle % (2 * math.pi)
+        # Schedule the next update
+        master.canvas.after(20, lambda : move_circle(master))
+def start_stopwatch(master, root):
+    global sw_stop_flag
+    if not master.sp_start_flag:
+        master.sp_start_flag=True
+    if sw_stop_flag:
+        sw_stop_flag=False
+    
+        
+    master.reset_button.grid(row=0, column=1)
+    master.stop_watchBtn.config(width=15)
+    change_theme(master,root)
+    if not master.is_moving:
+        master.is_moving = True
+    threading.Thread(target=stopwatch, args=(master,root)).start()
+def stop_stopwatch(master, root):
+    master.is_moving = False
+    global sw_stop_flag
+    if master.sp_start_flag:
+        master.sp_start_flag=False
+    if not sw_stop_flag:
+        sw_stop_flag=True
+    change_theme(master,root)
+    
+def stopwatch(master,root):
+    
+    move_circle(master)
+    while not sw_stop_flag:
+        master.sw_centi.set(("0"+str(int(master.sw_centi.get())+1))if len(str(int(master.sw_centi.get())+1))==1 else str(int(master.sw_centi.get())+1))
+        time.sleep(0.014)
+        if int(master.sw_centi.get()) ==60:
+            master.sw_second.set(("0"+str(int(master.sw_second.get())+1))if len(str(int(master.sw_second.get())+1))==1 else str(int(master.sw_second.get())+1))
+            master.sw_centi.set("00")
+            if int(master.sw_second.get())==60:
+                master.sw_second.set("00")
+                master.sw_minute.set(("0"+str(int(master.sw_minute.get())+1))if len(str(int(master.sw_minute.get())+1))==1 else str(int(master.sw_minute.get())+1))
+        root.update_idletasks()
+def reset_sw(master):
+    global sw_stop_flag
+    if sw_stop_flag:
+        master.sw_centi.set("00")
+        master.sw_second.set("00")
+        master.sw_minute.set("00")
--- UI.py
+++ UI.py
@@ -3,52 +3,60 @@ from tkinter import ttk
 from Function import *
 from PIL import Image, ImageTk
 from Function import minimize_window
+import math
 
 # create interface
 
 
-
 class UI:
     def __init__(self, root):
-        root.title("Clock")
         
-        root.geometry("900x400")
-        global timer_icon, stop_watch,start_icon,stop_icon,close,minimize, maximize
+        self.root = root
+        root.title("Clock")
+        root.geometry("900x450")
+        #Timer Tab_UI
+        global timer_icon, stop_watch, start_icon, stop_icon, close, minimize, maximize,reset,pause_sp
+        self.sp_start_flag =False
+
         self.minutes = StringVar()
         self.second = StringVar()
-        self.theme_value = BooleanVar()   
-        timer_icon = self.get_img("Assets\\hourglass.png", 25, 25)
-        stop_watch = self.get_img("Assets\\stopwatch.png", 30, 30)
-        close = self.get_img("Assets\\pause _icon.png", 25,25)
-        minimize = self.get_img("Assets\\minus.png",25,25)
-        maximize = self.get_img("Assets\\maximize-2.png",25,25)
-
+        self.sw_minute=StringVar()
+        self.sw_second=StringVar()
+        self.sw_centi=StringVar()
+        self.theme_value = BooleanVar()
+        timer_icon = get_img("Assets\\hourglass.png", 25, 25)
+        stop_watch = get_img("Assets\\stopwatch.png", 30, 30)
+        close = get_img("Assets\\pause _icon.png", 25, 25)
+        minimize = get_img("Assets\\minus.png", 25, 25)
+        maximize = get_img("Assets\\maximize-2.png", 25, 25)
+        reset =get_img("Assets\\reset.png",20,20)
+        pause_sp = get_img("Assets\\pause_sp.png",20,20)
         self.notebook = ttk.Notebook(root)
         self.notebook.pack(fill=BOTH, expand=True)
-        #custom title bar
-        self.title_frame = Frame(self.notebook,relief="flat", border=0)
-        self.title_frame.pack(side=TOP, anchor=E,pady=8)
-        self.close =Button(self.title_frame, text='',
-                               image=close,
-                               
-                               relief='flat',
-                               activebackground="#e7e7e7",
-                               border=0,
-                               width=30,
-                               command=lambda: close_win(root))
+        # custom title bar
+        self.title_frame = Frame(self.notebook, relief="flat", border=0)
+        self.title_frame.pack(side=TOP, anchor=E, pady=8)
+        self.close = Button(self.title_frame, text='',
+                            image=close,
+
+                            relief='flat',
+                            activebackground="#e7e7e7",
+                            border=0,
+                            width=30,
+                            command=lambda: close_win(root))
         self.close.grid(row=0, column=2)
         self.minimize = Button(self.title_frame, text='', image=minimize,
-                               
+
                                relief='flat',
                                activebackground="#e7e7e7",
-                               border=0,width=30,
+                               border=0, width=30,
                                command=lambda: minimize_window(root))
         self.minimize.grid(row=0, column=0)
-        self.maximize=Button(self.title_frame, text='', image=maximize,
-                               
+        self.maximize = Button(self.title_frame, text='', image=maximize,
+
                                relief='flat',
                                activebackground="#e7e7e7",
-                               border=0,width=30,
+                               border=0, width=30,
                                command=lambda: maximize_win(root))
         self.maximize.grid(row=0, column=1)
         # create timer tab
@@ -56,8 +64,9 @@ class UI:
         self.notebook.add(tab1, text="Timer", image=timer_icon, compound=LEFT)
 
         # create stopwatch tab
-        tab2 = Frame(self.notebook)
-        self.notebook.add(tab2, text="Stop Watch", image=stop_watch, compound=LEFT)
+        self.tab2 = Frame(self.notebook)
+        self.notebook.add(self.tab2, text="Stop Watch",
+                          image=stop_watch, compound=LEFT)
 
         # first half frame
         tab1_firstFm = Frame(tab1)
@@ -71,11 +80,11 @@ class UI:
         timer_frame = Frame(tab1_firstFm)
         timer_frame.pack(side="bottom")
 
-        #switch theme
+        # switch theme
         theme = ttk.Checkbutton(tab1_firstFm, text="Dark Mode", style="Switch.TCheckbutton", variable=self.theme_value,
                                 command=lambda: change_theme(self, root))
-        theme.pack(side= RIGHT, padx=5)
-        
+        theme.pack(side=RIGHT, padx=5)
+
         # minute frame
         minute_frame = Frame(timer_frame)
         minute_frame.grid(row=1, column=1)
@@ -89,7 +98,7 @@ class UI:
             minute_frame,
             font=("Roboto mono", 50),
             width=5,
-            
+
             justify=RIGHT,
             textvariable=self.minutes
         )
@@ -97,81 +106,126 @@ class UI:
         Label(minute_frame, text="m", font=("Roboto mono", 20)).grid(
             row=0, column=1, sticky=S
         )
-        self.minute_ui.bind("<Button-1>", lambda e: self.minute_ui.delete(0,tk.END))
-        self.minute_ui.bind("<Key>", lambda e: limit_digit(self,self.minutes))
-        
+        self.minute_ui.bind(
+            "<Button-1>", lambda e: self.minute_ui.delete(0, tk.END))
+        self.minute_ui.bind("<Key>", lambda e: limit_digit(self, self.minutes))
+
         # second UI that allow user to input second
         self.second_ui = ttk.Entry(
             second_frame,
             font=("Roboto mono", 50),
             width=5,
-            
+
             justify=RIGHT,
             textvariable=self.second,
-            
+
         )
         self.second_ui.grid(row=0, column=0)
         Label(second_frame, text="s", font=("Roboto mono", 20)).grid(
             row=0, column=1, sticky=S
         )
-        self.second_ui.bind("<Button-1>", lambda e: self.second_ui.delete(0,tk.END))
-        self.second_ui.bind("<Key>", lambda e: limit_digit(self,self.second,True))
+        self.second_ui.bind(
+            "<Button-1>", lambda e: self.second_ui.delete(0, tk.END))
+        self.second_ui.bind(
+            "<Key>", lambda e: limit_digit(self, self.second, True))
         # get icons
-        
-        start_icon = self.get_img(img="Assets\\play-button_724963.png")
-        stop_icon = self.get_img(img="Assets\\pause _icon.png")
+
+        start_icon = get_img(img="Assets\\play-button_724963.png")
+        stop_icon = get_img(img="Assets\\pause _icon.png")
 
         # control frame(start + stop)
         control_frame = Frame(tab1_secondFm)
         control_frame.pack(side="bottom", anchor="w", padx=10, pady=20)
-        #create Timer_object
+        # create Timer_object
         timer_obj = Timer(self, root)
 
         self.start_button = ttk.Button(
             control_frame, text="Start", width=15, image=start_icon, compound=LEFT,
-            command=lambda: timer_obj.start(),#style="Accent.TButton",
-            padding= (1,1)
+            command=lambda: timer_obj.start(),
+            padding=(1, 1)
         )
         self.start_button.grid(row=0, column=0)
 
         self.stop_button = ttk.Button(
-            control_frame, text="Stop", width=15, image=stop_icon, compound=LEFT, 
+            control_frame, text="Stop", width=15, image=stop_icon, compound=LEFT,
             command=lambda: timer_obj.stop(),
-            padding= (1,1)
+            padding=(1, 1)
         )
         self.stop_button.grid(row=0, column=1)
-        self.notebook.bind('<<NotebookTabChanged>>', lambda e: change_theme(self, root))
+        self.notebook.bind('<<NotebookTabChanged>>',
+                           lambda e: change_theme(self, root))
         
+        #Timer Stopwatch Tab_UI
+        self.canvas = Canvas(self.tab2, width=900, height=350)
+        self.canvas.pack(expand=True)
+    
+        self.path=self.canvas.create_oval(296,329,604,21,outline="grey", width=5)
+        self.circle =self.canvas.create_oval(434.6,344.4,465.4,313.6, fill='light grey', outline='light grey')
+        # Extract the center and radius of the path
+        self.center_x, self.center_y, self.radius = calculate_path_details(self)
+        # Set the initial angle for the circle
+        self.angle = 0
+        # Define the speed of the circle
+        self.speed = 0.035
+        
+        self.is_moving = False
+        
+        # add control
+        self.sp_control = Frame(self.tab2)
+        self.sp_control.pack(pady=5)
 
-    def get_img(self, img, width=20, height=20):
-        start_img = Image.open(img)
-        start_resize = start_img.resize((width, height))
-        final = ImageTk.PhotoImage(start_resize)
-        return final
+        self.stop_watchBtn = ttk.Button(self.sp_control,
+                                        text='',image=start_icon,
+                                        compound='center',
+                                        width=30, 
+                                        command=lambda : start_stopwatch(self,root) if not self.sp_start_flag else stop_stopwatch(self,root))
+        self.stop_watchBtn.grid(row=0, column=0)
+        self.reset_button = ttk.Button(self.sp_control, text='', image=reset, compound='center',width =15, command=lambda: reset_sw(self))
+        
+        #stop watch label
+        stop_watch_frame = Frame(self.canvas)
+        stop_watch_frame.place(relx=0.37, rely=0.4)
+        self.minute_label = Label(stop_watch_frame, font=("Roboto mono", 35), textvariable=self.sw_minute)
+        self.minute_label.grid(row=0, column=0)
+
+        self.second_label = Label(stop_watch_frame, font=("Roboto mono", 35), textvariable=self.sw_second)
+        self.second_label.grid(row=0, column=2)
+
+        self.centisecond = Label(stop_watch_frame, font=("Roboto mono", 35), textvariable=self.sw_centi)
+        self.centisecond.grid(row=0, column=4)
+        for i in range(2):
+            Label(stop_watch_frame, text=":", font=("Roboto mono", 35)).grid(row=0, column=(i*2)+1)
+        self.sw_centi.set("00")
+        self.sw_second.set("00")
+        self.sw_minute.set("00")
 
 
-if __name__ == "__main__":
+def main():
     window = Tk()
-    #move window without title bar
+    # move window without title bar
+
     def move(event):
-        
-        x= window.winfo_x()-window.startX + event.x
+
+        x = window.winfo_x()-window.startX + event.x
         y = window.winfo_y()-window.startY + event.y
         window.geometry(f'+{x}+{y}')
-    
+
     def origin_cords(event):
-        window.startX= event.x
-        window.startY= event.y
+        window.startX = event.x
+        window.startY = event.y
     
     window.overrideredirect(True)
 
     window.bind('<Button-1>', origin_cords)
-    window.bind('<B1-Motion>',move)
-    ui= UI(window)
-    
+    window.bind('<B1-Motion>', move)
+    ui = UI(window)
+
     ui.minutes.set("00")
     ui.second.set("00")
-    change_theme(ui,window)
-    
+    change_theme(ui, window)
     
     window.mainloop()
+
+
+if __name__ == "__main__":
+    main()
